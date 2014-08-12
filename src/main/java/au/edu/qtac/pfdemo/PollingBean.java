1d46b08001849e1cc408f792d84d8a9504ba239e
unknown <peter-wei.cai@QTAC-02254.qtac.edu.au>
2014-08-12 15:56:40 +1000

    demo ConversationScoped bean with Poll

--- src/main/java/au/edu/qtac/pfdemo/PollingBean.java
+++ src/main/java/au/edu/qtac/pfdemo/PollingBean.java
@@ -6,20 +6,49 @@
 
 package au.edu.qtac.pfdemo;
 
-import javax.enterprise.context.SessionScoped;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
- *
+ *  See http://www.byteslounge.com/tutorials/java-ee-cdi-conversationscoped-example
+ * It demonstrates the usage of ConversationScoped bean.
  * @author peter
  */
 @Named
-@SessionScoped
+@ConversationScoped
 public class PollingBean implements java.io.Serializable {
     private int count;
     
+    @Inject
+    private Conversation c;
+    
+    @PostConstruct
+    public void init() {
+        System.out.println("=====new Bean instances created======");
+    }
+    
+    public void startConversation() {
+       System.out.println("=====in startConversation method");
+       if(!FacesContext.getCurrentInstance().isPostback() && c.isTransient()) {
+            System.out.println("===start a long conversation==");
+           c.begin();
+           System.out.println("cid=" + c.getId());
+       }   
+    }
+
     public void doIt() {
         count++;
         System.out.println(this.hashCode() + "count:" + count);
+    }
+    
+    public void endConversation() {
+        if(!c.isTransient()) {
+            c.end();
+            System.out.println("cid=" + c.getId() + ", ended.");
+        }
     }
 }
