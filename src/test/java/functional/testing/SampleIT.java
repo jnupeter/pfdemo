0adf723502d75eb899b2fcd34413d95ba33e7b0f
Peter Cai <jnupeter@gmail.com>
2014-10-26 12:58:04 +1000

    add integration testing

---
+++ src/test/java/functional/testing/SampleIT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package functional.testing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author peter
+ */
+public class SampleIT {
+    
+    @Test
+    public void test() {
+        System.out.println("integration test in progress ........");
+        assertTrue(true);
+    }
+    
+}
